4342e9a0b56b5344a2c7f2fc018f2ca6a750d073
annaDsw <dsw46895@student.dsw.edu.pl>
2022-07-02 21:25:28 +0200

    Adding --logs to skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -6,3 +6,19 @@ date()
 {
 	echo "Today is $day"
 }
+
+logs()
+{
+	if [ "$1" -ge "0" ]; then
+		n=$1
+	else
+		n=100
+	fi
+	i=1
+	while (($i <= $n)); do
+		echo -e "Nazwa pliku: log$i.txt\\n">log$i.txt
+		echo -e "Nazwa skryptu który uwtorzył ten plik: $0\\n">>log$i.txt
+		echo -e "Data utworzenia: $day\\n">>log$i.txt
+		let "$[i++]"
+	done
+}
